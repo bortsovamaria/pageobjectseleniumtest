bec233232cefbd711ec4c7d48e368cb4234a878a
lutteur <bortsovamariia@gmail.com>
2019-06-20 23:09:20 +0300

    Add test at LoginPage

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MainPage {
+
     private WebDriver driver;
 
     public MainPage(WebDriver driver) {
---
+++ src/test/java/LoginPageTest.java
@@ -0,0 +1,37 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginPageTest {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/home/o12345/IdeaProjects/pageobjectseleniumtest/drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://github.com/login");
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void loginWithEmptyCredsTest() {
+        LoginPage newLoginPage = loginPage.loginWithInvalidCreds("", "");
+        String error = newLoginPage.getErrorTextLogin();
+        Assert.assertEquals("Incorrect username or password.", error);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
